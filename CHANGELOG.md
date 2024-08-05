4bae66d15a9875e16ff4e2b8f3037249ebd83fd4
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-08-05 15:56:38 +0200

    add compare

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,10 @@ Please choose versions by [Semantic Versioning](http://semver.org/).
 * MINOR version when you add functionality in a backwards-compatible manner, and
 * PATCH version when you make backwards-compatible bug fixes.
 
+## v1.3.0
+
+- Add compare time
+
 ## v1.2.0
 
 - Allow ParseTimeOfDay with Timezone. Example '13:37 Europe/Berlin'
---
+++ time_compare_test.go
@@ -0,0 +1,22 @@
+// Copyright (c) 2024 Benjamin Borbe All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package time_test
+
+import (
+	"time"
+
+	libtime "github.com/bborbe/time"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = DescribeTable("Compare",
+	func(a time.Time, b time.Time, expectedResult int) {
+		Expect(libtime.Compare(a, b)).To(Equal(expectedResult))
+	},
+	Entry("equal", time.Unix(1000, 0), time.Unix(1000, 0), 0),
+	Entry("less", time.Unix(999, 0), time.Unix(1000, 0), -1),
+	Entry("greater", time.Unix(1000, 0), time.Unix(999, 0), 1),
+)
