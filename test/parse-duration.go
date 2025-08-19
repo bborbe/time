0132629591afb1f2793f2edb11889b925879f6b8
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-08-19 23:12:23 +0200

    add test parse duration

---
+++ test/parse-duration.go
@@ -0,0 +1,18 @@
+// Copyright (c) 2023 Benjamin Borbe All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package test
+
+import (
+	"context"
+	. "github.com/onsi/gomega"
+
+	libtime "github.com/bborbe/time"
+)
+
+func ParseDuration(value interface{}) libtime.Duration {
+	result, err := libtime.ParseDuration(context.Background(), value)
+	Expect(err).To(BeNil())
+	return *result
+}
